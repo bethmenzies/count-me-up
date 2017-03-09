c97db583ec82c19392a953ec520c12095601dd0b
Beth Menzies <BethM@Beths-MacBook-Pro.local>
2017-03-09 12:25:53 +0000

    Adding bootstrap styling to create cleaner pages

--- controllers/votes-controller.js
+++ controllers/votes-controller.js
@@ -1,4 +1,5 @@
 countMeUp.controller("VotesController", ['$scope', '$routeParams', '$location', function($scope, $routeParams, $location) {
 	$scope.numberOfVotes = $routeParams.numberOfVotes;
 	var candidatePercentages = $location.search();
+	console.log(candidatePercentages);
 }]);
