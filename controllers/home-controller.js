450a4e984b091ed0a6d1269c5b21665d2e63700c
Beth Menzies <BethM@Beths-MacBook-Pro.local>
2017-03-09 22:43:29 +0000

    first working version

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -1,2 +1,11 @@
 countMeUp.controller("HomeController", function($scope) {
+	$scope.getVoteCount = function() {
+		$scope.showVotes = true;
+		$scope.voteCount = {
+			"candidate-1": 30,
+			"candidate-2": 40,
+			"candidate-3": 20,
+			"candidate-4": 10
+		};
+	};
 });
--- controllers/votes-controller.js
+++ controllers/votes-controller.js
@@ -1,5 +1,12 @@
 countMeUp.controller("VotesController", ['$scope', '$routeParams', '$location', function($scope, $routeParams, $location) {
 	$scope.numberOfVotes = $routeParams.numberOfVotes;
 	var candidatePercentages = $location.search();
-	console.log(candidatePercentages);
+	$scope.getVoteCount = function() {
+		$scope.showVotes = true;
+		var voteCount = {};
+		for (var candidate in candidatePercentages) {
+			voteCount[candidate] = ($scope.numberOfVotes / 100)*candidatePercentages[candidate]; 
+		}
+		$scope.voteCount = voteCount;
+	};
 }]);
